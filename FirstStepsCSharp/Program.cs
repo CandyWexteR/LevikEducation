b336fc775f136a3f8733e8876d56fb18b728d6f4
shoot <mr.fostik@mail.ru>
2023-01-15 12:00:58 +0700

    Типы данных

--- FirstStepsCSharp/Program.cs
+++ FirstStepsCSharp/Program.cs
@@ -1,9 +1,34 @@
-﻿namespace FirstStepsCSharp;
+﻿using System.Runtime.CompilerServices;
+
+namespace FirstStepsCSharp;
 
 internal static class Program
 {
     public static void Main(string[] args)
     {
+        /*
+         Значимые типы данных
+         int
+         long
+         char
+         bool
+         float
+         double
+         e.t.c.
+         //string
+         Ссылочные
+         К ссылочным относят классы
+         Type,
+         Enum,
+         e.t.c.
+         */
         
+        
+        //Банальный пример значимого типа
+        int a = 52;
+        double b = 52.285;
+        
+        //Банальный пример ссылочного
+        Type type = typeof(Program);
     }
 }
